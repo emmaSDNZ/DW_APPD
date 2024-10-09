0ce8073c6e53effc152ee9089a6ef21fdaab7208
Emanuel <isaias.e.sudanez@gmail.com>
2024-10-09 18:50:38 -0300

    ejercicios operadores logicos

---
+++ Javascript/6_operadores_logicos.js
@@ -0,0 +1,70 @@
+/*
+
+
+**NIVEL BASICO
+
+
+Ejercicio 1: Evaluación de Verdadero/Falso
+
+Descripción: Crea una función llamada evaluarVerdadero que reciba un booleano y devuelva:
+"Es verdadero" si el valor es true y "Es falso" si es false.
+
+utiliza el operador ternario.
+
+Ejercicio 2: Uso del operador AND (&&)
+
+Descripción: Crea una función llamada esMayorDeEdad que reciba dos parámetros: 
+edad y tieneIdentificacion. La función debe devolver true si edad es mayor o igual a 18 y 
+tieneIdentificacion es true. En caso contrario, devuelve false.
+
+Ejercicio 3: Uso del operador OR (||)
+
+Descripción: Crea una función llamada puedeAcceder que reciba dos parámetros: esMiembro y 
+tieneInvitacion. La función debe devolver true si cualquiera de las dos condiciones es verdadera.
+
+
+Ejercicio 4: Uso del operador NOT (!)
+
+Descripción: Crea una función llamada noEsVerdadero que reciba un booleano y devuelva su negación.
+
+
+**NIVEL INTERMEDIO
+
+
+Ejercicio 5: Comparación de resultados con AND (&&)
+
+Descripción: Crea una función llamada esPositivo que reciba un número y devuelva true si el número es
+mayor que 0 y es par. Luego, crea otra función llamada compararResultados que llame a esPositivo y 
+evalúe si el resultado es true o false.
+
+Ejercicio 6: Comparación de resultados con OR (||)
+
+Descripción: Crea una función llamada esValido que reciba un número y devuelva true si es mayor que 10 o
+es igual a 5. Luego, crea otra función llamada compararResultadosValido que evalúe si el resultado es 
+true o false.
+
+Descripción: Crea una función llamada validarUsuario que reciba tres parámetros: nombre, edad, y esAdmin. 
+La función debe devolver true si el nombre no está vacío, edad es mayor o igual a 18 y esAdmin es true. 
+En caso contrario, devuelve false.
+
+
+**NIVEL AVANZADO
+
+
+Descripción: Crea una función llamada evaluarCondiciones que reciba tres booleanos: condicion1, condicion2,
+y condicion3. La función debe devolver true si al menos una condición es verdadera, pero no más de dos. 
+Usa operadores Lógicos para implementarlo.
+
+
+Combinación de operadores
+
+Descripción: Crea una función llamada analizarNumero que reciba un número y devuelva true si es positivo, 
+par, y menor que 100.  Devuelve false en caso contrario.
+
+Ejercicio 10: Condiciones anidadas
+
+Descripción: Crea una función llamada verificarAcceso que reciba un objeto con las propiedades edad, tienePermiso,
+y rol. La función debe devolver true si la edad es mayor o igual a 18, tienePermiso es true, o si el rol
+es "admin".
+
+*/
